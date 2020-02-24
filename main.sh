809111536d47a3df78626a628f77beb6d0a7de68
Dilan Ormisiar <dilanormisiar@gmail.com>
2020-02-24 13:08:28 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,24 @@
+echo Hello World
+
+City='New York';
+Numeric1=23
+Numeric2=45
+echo $City $Numeric1 + $Numeric2
+echo "what is name friend? "
+read name
+echo your name is $name
+
+let Meme=345-22
+echo $Meme
+echo here comes the calc
+RESULT2=$(expr 3 \* 3)
+echo $RESULT2
+
+if [ "$name="HI"""]
+  then 
+    echo "yes it worked"
+  else
+    echo "oh noes"
+fi
+
+#https://itnext.io/bash-scripting-everything-you-need-to-know-about-bash-shell-programming-cd08595f2fba
