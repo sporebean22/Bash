bb59c8df67f162b489193dd614ecc3a06f237e24
Dilan Ormisiar <dilanormisiar@gmail.com>
2020-02-26 16:32:27 +0000

    lmeyo

---
+++ Mee.sh
@@ -0,0 +1,36 @@
+
+echo EYO > Testo.txt
+
+City='New York';
+Numeric1=23
+Numeric2=45
+echo $City $Numeric1 + $Numeric2
+echo what is name friend?
+read name
+echo your name is $name
+
+let Meme=345-22
+echo $Meme
+echo here comes the calc
+RESULT2=$(expr 3 \* 3)
+echo $RESULT2
+
+function Yeet()
+{
+  expr 5*8
+  echo $
+}
+
+if [ $name=HI]
+  then 
+    echo yes it worked
+  else
+    echo oh noes
+fi
+
+#https://dev.to/awwsmm/101-bash-commands-and-tips-for-beginners-to-experts-30je
+
+#https://itnext.io/bash-scripting-everything-you-need-to-know-about-bash-shell-programming-cd08595f2fba
+
+#https://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html
+"
